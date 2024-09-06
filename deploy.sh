c3730911c0ec0420add483cd575c4e62bc228c4a
emilsonsn <emilsonsn2@gmail.com>
2024-09-06 09:43:08 -0300

    feat: adiciona arquivo deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+gti pull https://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@github.com/emilsonsn/licitador-api.git
+
+composer update
+
+php artisan key:generate
+
+php artisan migrate
+
+php artisan db:seed
+
+php artisan optimize
+
+php artisan optimize:clear
