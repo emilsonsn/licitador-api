f1856aabadbaf47f5eeb90a9ffaace8ffc676536
emilsonsn <emilsonsn2@gmail.com>
2025-02-01 11:52:04 -0300

    fix: protege rotinas contra sobreposição

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:alerta-licitacao-search')->dailyAt('19:00');
-        $schedule->command('app:pncp-search')->everyThreeHours();
+        $schedule->command('app:pncp-search')->everyThreeHours()->withoutOverlapping();
         $schedule->command('app:pncp-get-items')->dailyAt('09:00');
         $schedule->command('app:document-notification-whatsapp')->dailyAt('18:00');
         $schedule->command('app:send-notification-whatsapp')->cron('0 19 */2 * *');  
