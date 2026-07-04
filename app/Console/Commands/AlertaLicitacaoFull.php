c81aead331c77b87c0d0c7bb3763f9f9a4782d51
Emilson de Souza Nascimento <emilsonsn@users.noreply.github.com>
2026-07-04 00:14:31 -0300

    feat: implementa comando com modalidade 2 do alerta

---
+++ app/Console/Commands/AlertaLicitacaoFull.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\Routines\RoutinesService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class AlertaLicitacaoFull extends Command
+{
+
+    private $routineService;
+
+    public function __construct(RoutinesService $routineService) {
+        parent::__construct(); 
+        $this->routineService = $routineService;
+    }
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:alerta-licitacao-full';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->routineService->populate_database_alerta_licitacao([
+            'modalidade' => '2,5,6,8',
+            'pagina' => 1,
+            'id_portal' => 9999,
+            'data_insercao' => Carbon::today()->format('Y-m-d'),
+        ]);
+    }
+}
